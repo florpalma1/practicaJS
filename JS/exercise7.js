b295cfcbcda1aa574aed820e6e828859baba4bf1
florpalma1 <contr.lucascordoba98@gmail.com>
2023-03-01 12:34:10 -0300

    agregue ejercicio 7

--- JS/exercise7.js
+++ JS/exercise7.js
@@ -4,3 +4,12 @@ let number1 = parseInt(prompt("Enter a number"));
 let number2 = parseInt(prompt("Enter other number"));
 let number3 = parseInt(prompt("Enter other number"));
 
+if (number1 > number2 && number1 > number3){
+    document.write("The first number entered is the largest")
+} else if (number2 > number1 && number2 > number3) {
+    document.write("The second number entered is the largest")
+} else if (number3 > number1 && number3 > number2){
+    document.write("The third number entered is the largest")
+} else {
+    document.write("All the numbers entered are the same")
+}
