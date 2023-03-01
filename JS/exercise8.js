0266a37db005497f498b4e6a2d9a1aeff73044ef
florpalma1 <contr.lucascordoba98@gmail.com>
2023-03-01 12:48:28 -0300

    agregue ejercicio 8 utilizando operador matematico % y ===

---
+++ JS/exercise8.js
@@ -0,0 +1,9 @@
+// 8.- Escribe un programa que pida un número y diga si es divisible por 2
+
+let number = parseInt(prompt("Please enter a number"));
+
+if (number % 2 === 0 ) {
+    document.write("The number entered is divisible by two")
+} else {
+    document.write("The number entered is not divisible by two")
+}; 
